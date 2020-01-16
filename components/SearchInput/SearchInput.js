b9fa27a7594a94267e08359f0da6131795c2183a
Alice-cxy <2584005223@qq.com>
2020-01-16 17:48:57 +0800

    搜索组件完成

---
+++ components/SearchInput/SearchInput.js
@@ -0,0 +1,26 @@
+// components/SearchInput/SearchInput.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    searchText:{ 
+      type:String,
+      value:"搜索"
+      }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
